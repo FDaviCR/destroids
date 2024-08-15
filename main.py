6b8bd8113a939a03d9f4c13f6513c85b94f2ccb2
Davi <davicamiloribeiro@gmail.com>
2024-08-15 20:29:40 -0300

    Ajustes Iniciais

--- main.py
+++ main.py
@@ -2,33 +2,33 @@ import pygame;
 import sys;
 
 # Inicializa o Pygame
-pygame.init();
+pygame.init()
 
 # Definir as dimensões da tela
-screen_width = 800;
-screen_height = 600;
-screen = pygame.display.set_mode((screen_width, screen_height));
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Definir a cor de fundo (RGB)
-background_color = (0, 0, 0);
+background_color = (0, 0, 0)
 
 # Definir o título da janela
-pygame.display.set_caption('Tela Inicial');
+pygame.display.set_caption('Tela Inicial')
 
 # Loop principal do jogo
-running = True;
+running = True
 while (running):
     # Preenche a tela com a cor de fundo
-    screen.fill(background_color);
+    screen.fill(background_color)
 
     # Lidar com eventos (como fechar a janela)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False;
+            running = False
 
     # Atualizar a tela
-    pygame.display.flip();
+    pygame.display.flip()
 
 # Sair do Pygame
-pygame.quit();
-sys.exit();
+pygame.quit()
+sys.exit()
